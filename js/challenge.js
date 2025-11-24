fd46129bdef1a35ca3c5d1b0e312a717b36ee26d
Kelly Maass <kmaass77627@gmail.com>
2025-11-23 18:34:05 -0600

    changes

--- js/challenge.js
+++ js/challenge.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const pause = document.getElementById("pause");
     const minus = document.getElementById("minus");
     const plus = document.getElementById("plus");
+    const heart = document.getElementById("heart");
     let count = 0;
 
 
@@ -16,10 +17,25 @@ let timer = setInterval(() => {
 }, 1000);
 
 pause.addEventListener("click", () => {
-    if
+    clearInterval(timer);
+    pause.textContent = "resume";
 })
 
-minus.addEventListener("click", () => {})
+minus.addEventListener("click", () => {
+    count--;
+    counter.textContent = count;
+})
+
+plus.addEventListener("click", () => {
+    count ++;
+    counter.textContent = count;
+})
+
+heart.addEventListener("click", () => {
+    const likeList = document.querySelector(".likes");
+    let like = document.querySelector();
+    likeList.appendChild(like);
+});
 
 
 });
