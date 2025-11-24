8ffd1b080407b1d6075ecbe2e7ee651dba0fb418
Kelly Maass <kmaass77627@gmail.com>
2025-11-24 10:33:53 -0600

    done

--- js/challenge.js
+++ js/challenge.js
@@ -1,41 +1,76 @@
 
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const counter = document.getElementById("counter");
     const pause = document.getElementById("pause");
     const minus = document.getElementById("minus");
     const plus = document.getElementById("plus");
     const heart = document.getElementById("heart");
+    const restart = document.getElementById("restart"); 
     let count = 0;
+    let timer = startTimer();
+    let isPaused = false;
+
+    function startTimer() {
+        return setInterval(() => {
+            count++;
+            counter.textContent = count;
+        }, 1000);
+    }
+
+   
+    pause.addEventListener("click", () => {
+        if (!isPaused) {
+            clearInterval(timer);
+            pause.textContent = "resume";
+            minus.disabled = true;
+            plus.disabled = true;
+            heart.disabled = true;
+        } else {
+            timer = startTimer();
+            pause.textContent = "pause";
+            minus.disabled = false;
+            plus.disabled = false;
+            heart.disabled = false;
+        }
+
+        isPaused = !isPaused;
+    });
+
+    
+    minus.addEventListener("click", () => {
+        count--;
+        counter.textContent = count;
+    });
 
 
-let timer = setInterval(() => {
-   count++;
-   counter.textContent = count;
-}, 1000);
+    plus.addEventListener("click", () => {
+        count++;
+        counter.textContent = count;
+    });
 
-pause.addEventListener("click", () => {
-    clearInterval(timer);
-    pause.textContent = "resume";
-})
 
-minus.addEventListener("click", () => {
-    count--;
-    counter.textContent = count;
-})
+    heart.addEventListener("click", () => {
+        const likeList = document.querySelector(".likes");
+        let li = document.createElement("li");
+        li.textContent = `${count} has been liked 1 time`;
+        likeList.appendChild(li);
+    });
 
-plus.addEventListener("click", () => {
-    count ++;
-    counter.textContent = count;
-})
+    
+    restart.addEventListener("click", () => {
+        clearInterval(timer);
+        count = 0;
+        counter.textContent = 0;
+        isPaused = false;
 
-heart.addEventListener("click", () => {
-    const likeList = document.querySelector(".likes");
-    let like = document.querySelector();
-    likeList.appendChild(like);
-});
+        
+        minus.disabled = false;
+        plus.disabled = false;
+        heart.disabled = false;
+        pause.textContent = "pause";
 
+        timer = startTimer();
+    });
 
 });
